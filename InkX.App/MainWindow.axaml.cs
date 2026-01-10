f553bc5806f9de51a55a1f1ffdecf83c8d421e18
GeorgiEnev <gogoenev@gmail.com>
2026-01-10 23:46:20 +0200

    add drawing canvas and pointer event hooks

--- InkX.App/MainWindow.axaml.cs
+++ InkX.App/MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using Avalonia.Input;
 
 namespace InkX.App
 {
@@ -7,6 +8,26 @@ namespace InkX.App
         public MainWindow()
         {
             InitializeComponent();
+
+            DrawCanvas.PointerPressed += OnPointerPressed;
+            DrawCanvas.PointerMoved += OnPointerMoved;
+            DrawCanvas.PointerReleased += OnPointerReleased;
         }
+
+        private void OnPointerPressed(object? sender, PointerPressedEventArgs e)
+        {
+            // This will run when mouse/pen goes down
+        }
+
+        private void OnPointerMoved(object? sender, PointerEventArgs e)
+        {
+            // This will run when mouse/pen moves
+        }
+
+        private void OnPointerReleased(object? sender, PointerReleasedEventArgs e)
+        {
+            // This will run when mouse/pen goes up
+        }
+
     }
 }
